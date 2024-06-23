de90189f30e57f7cf8892e74bce9633afcb07e24
Leo1998 <leonard.fricke98@gmail.com>
2024-06-23 11:59:34 +0200

    fix in midi generator script

--- misc/generate_midis.py
+++ misc/generate_midis.py
@@ -1,19 +1,26 @@
 from multiprocessing import Pool
-import os
+import subprocess
 from AugmentedNet.common import ANNOTATIONSCOREDUPLES
+import os
 import music21
+
+musescore_bin = "/home/god/Downloads/MuseScore-Studio-4.3.1.241490902-x86_64.AppImage"
 
 def generate(inn, out):
     print(f"Converting file: {inn} to {out}")
     try:
         score = music21.converter.parse(inn, format="musicxml", forceSource=True)
-
         #remove all repeats
         for r in score.recurse().getElementsByClass("RepeatMark"):
             score.remove(r, recurse=True)
-        
+        out_mxl = score.write("musicxml")
+        #score.write("midi", fp=out)
+
         os.makedirs(os.path.dirname(out), exist_ok=True)
-        score.write("midi", fp=out)
+
+        env = os.environ.copy()
+        env["SKIP_LIBJACK"] = "1"
+        subprocess.run([musescore_bin, out_mxl, "-o", out], env=env)
     except Exception as e:
         print(e)
 
